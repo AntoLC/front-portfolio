3facf925b01d343316f74466d5fd040ff162353b
ALC Consulting <freelance.anthony@gmail.com>
2021-10-28 17:04:38 +0200

    Improve Capsule Logo

--- src/pages/frontpage/Frontpage.tsx
+++ src/pages/frontpage/Frontpage.tsx
@@ -9,8 +9,9 @@ export const Frontpage = () => {
             <div className="font-sans text-2xl text-gray-800 font-black mx-5">Capsule</div>
         </div>
         <div className="form-top-right"/>
+        <Capsule isLogo={true} />
         <div className="h-screen flex flex-wrap content-center justify-center -m-10">
-            <Capsule/>
+            <Capsule isLogo={false} />
             <div className="font-sans text-8xl text-gray-800 font-black">Capsule</div>
         </div>
         <div className="form-bottom-left"/>
--- src/pages/frontpage/components/Capsule/Capsule.tsx
+++ src/pages/frontpage/components/Capsule/Capsule.tsx
@@ -1,13 +1,27 @@
 import react from "react";
 import './Capsule.css';
 
-export const Capsule = () => {
+interface CapsuleProps {
+    isLogo?: boolean;
+  }
+
+export const Capsule = (props: CapsuleProps) => {
+    const { isLogo } = props;
+
     return (
-    <div className="capsule relative -left-48">
-        <div className="capsule-bg w-24 h-24 rounded-lg z-0 transform rotate-45 absolute"/>
-        <div className="capsule-pills z-10 absolute left-16 -top-9">
-            <div className="capsule-top w-16 h-20 rounded-tl-full rounded-tr-full relative"/>
-            <div className="capsule-bottom w-16 h-20 rounded-bl-full rounded-br-full relative"/>
+    <div className={"capsule relative w-52 "+ (isLogo ? "capsule-logo" : "") }>
+        <div className="capsule-bg rounded-lg z-0 transform rotate-45 absolute"/>
+        <div className="capsule-pills z-10 absolute">
+            <div className="capsule-size capsule-top rounded-tl-full rounded-tr-full relative"/>
+            <div className="capsule-size capsule-bottom rounded-bl-full rounded-br-full relative"/>
+            <div className="capsule-powder absolute -right-6 top-1/3">
+                <div className="animate-pulse capsule-powder-1 capsule-powder-bg1 w-2 h-2 rounded-full absolute capsule-powder-1-abs"/>
+                <div className="animate-pulse capsule-powder-2 capsule-powder-bg2 w-2 h-2 rounded-full absolute capsule-powder-2-abs"/>
+                <div className="capsule-powder-3 capsule-powder-bg2 w-3 h-3 rounded-full absolute capsule-powder-3-abs"/>
+                <div className="animate-bounce capsule-powder-4 capsule-powder-bg1 w-4 h-4 rounded-full absolute capsule-powder-4-abs"/>
+                <div className="capsule-powder-5 capsule-powder-bg1 w-2 h-2 rounded-full absolute capsule-powder-5-abs"/>
+                <div className="animate-bounce capsule-powder-6 capsule-powder-bg2 w-2 h-2 rounded-full absolute capsule-powder-6-abs"/>
+            </div>
         </div>
     </div>
     );
