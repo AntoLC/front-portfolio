7860d7dd78ad216b95bcab0145dccc43a6833b6a
ALC Consulting <freelance.anthony@gmail.com>
2021-10-28 11:02:07 +0200

    add tailwind css

--- src/pages/frontpage/Frontpage.tsx
+++ src/pages/frontpage/Frontpage.tsx
@@ -1,5 +1,9 @@
 import react from "react";
 
 export const Frontpage = () => {
-    
+    return (
+    <div className="homepage font-sans">
+        Almost before we knew it, we had left the ground.
+    </div>
+    );
 }
