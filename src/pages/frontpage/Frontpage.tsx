8b5871ff15d6b31cfdb8e46d1e5cd6add59477ba
ALC Consulting <freelance.anthony@gmail.com>
2021-10-28 12:04:09 +0200

    - add scss - place element - handle tailwinds

--- src/pages/frontpage/Frontpage.tsx
+++ src/pages/frontpage/Frontpage.tsx
@@ -1,9 +1,12 @@
 import react from "react";
+import './frontpage.scss';
 
 export const Frontpage = () => {
     return (
-    <div className="homepage font-sans">
-        Almost before we knew it, we had left the ground.
+    <div className="frontpage-fkelfsed234 h-screen">
+        <div className="form-top-right absolute right-0 bg-gray-300 w-1/3">Block Top Right</div>
+        <div className="font-sans text-8xl text-gray-800 h-screen flex flex-wrap content-center justify-center">Quick Portfolio</div>
+        <div className="form-bottom-left absolute left-0 bottom-0 bg-gray-300 w-1/3">Block Bottom left</div>
     </div>
     );
 }
