8fbb89156e51d1fc572e4b54357449a16cf30379
ALC Consulting <freelance.anthony@gmail.com>
2021-10-29 16:12:01 +0200

    Made Menu-items

--- src/pages/Defenders/components/LeftContainer/LeftContainer.tsx
+++ src/pages/Defenders/components/LeftContainer/LeftContainer.tsx
@@ -1,19 +1,20 @@
 import './LeftContainer.scss';
 import logo_avenger from './img/logo-avenger.png';
+import { MenuItem } from './components/MenuItem/MenuItem';
 
 export const LeftContainer = () => {
     return (
-    <div className={ "LeftContainer-" + global.app_config.CSS_ID + " rounded-l-3xl p-6"}>
+    <div className={ "LeftContainer-" + global.app_config.CSS_ID + " rounded-l-3xl pt-6 pb-6"}>
         <div className="container-logo text-center mb-4">
             <img className="m-auto" src={logo_avenger} alt="logo_avenger" />
-            <div className="mt-2 font-sans uppercase font-bold site-name">Defenders</div>
-            <div className="font-sans text-gray-100 site-slogan">Guarding your event</div>
+            <p className="mt-2 font-sans uppercase font-bold site-name">Defenders</p>
+            <p className="font-sans text-gray-100 site-slogan">Guarding your event</p>
         </div>
-        <div className="container-menu">
-            <div className="logo-name">Overview</div>
-            <div className="logo-name">Recruitment</div>
-            <div className="logo-under">Agency</div>
-            <div className="logo-under">Log out</div>
+        <div className="container-menu mt-8">
+            <MenuItem svg_name="widgets" text="Overview"/>
+            <MenuItem svg_name="people" text="Recruitment"/>
+            <MenuItem svg_name="domain" text="Agency"/>
+            <MenuItem svg_name="logout" text="Log out"/>
         </div>
         <div className="container-bottom">
             <div className="logo-name">Overview</div>
